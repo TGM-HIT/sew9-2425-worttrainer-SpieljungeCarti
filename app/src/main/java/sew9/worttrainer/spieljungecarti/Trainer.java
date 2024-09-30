b14115352124be0106fccab5b19e9d01f6671ecb
SpieljungeCarti <thaas@student.tgm.ac.at>
2024-09-30 14:02:49 +0200

    Trainer v1

---
+++ app/src/main/java/sew9/worttrainer/spieljungecarti/Trainer.java
@@ -0,0 +1,44 @@
+package sew9.worttrainer.spieljungecarti;
+
+import java.util.List;
+import java.util.Random;
+
+public class Trainer {
+
+    private List<Pic> list;
+    private Pic current;
+    private Random random;
+    private int right;
+    private int amount;
+
+    public Trainer(List<Pic> list) {
+        this.list = list;
+        this.random = new Random();
+    }
+
+    public void select() {
+        if (list != null && !list.isEmpty()) {
+            int index = random.nextInt(list.size());
+            current = list.get(index);
+        }
+    }
+
+    public void select(int index) {
+        if (list != null && !list.isEmpty() && index >= 0 && index < list.size()) {
+            current = list.get(index);
+        }
+    }
+
+    public boolean check(String answer) {
+        amount++;
+        if (current.getName().toLowerCase() == answer.toLowerCase()) {
+            right++;
+            return true;
+        }
+        return false;
+    }
+
+    public String stats() {
+        return (right+ "/" +amount);
+    }
+}
